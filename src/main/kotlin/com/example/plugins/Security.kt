774c4af63bceb88831f95637fce62f31c55569fa
Mitchel Nijdam <mitchel.nijdam@teamrockstars.nl>
2021-11-28 14:05:52 +0100

    Configure basic auth + add README

--- src/main/kotlin/com/example/plugins/Security.kt
+++ src/main/kotlin/com/example/plugins/Security.kt
@@ -1,18 +1,26 @@
 package com.example.plugins
 
-import io.ktor.auth.*
-import io.ktor.util.*
 import io.ktor.application.*
+import io.ktor.auth.*
 import io.ktor.response.*
-import io.ktor.request.*
 import io.ktor.routing.*
 
 fun Application.configureSecurity() {
     install(Authentication) {
-        basic {}
+        basic("basic-auth") {
+            validate { credentials ->
+                // TODO: implement custom authentication mechanism
+                if (credentials.name == "java" && credentials.password == "magazine") {
+                    UserIdPrincipal(credentials.name)
+                } else {
+                    null
+                }
+            }
+        }
     }
+
     routing {
-        authenticate {
+        authenticate("basic-auth") {
             get("/authenticated") {
                 call.respondText("I am authenticated!")
             }
